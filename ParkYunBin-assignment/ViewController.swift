1a6b4aa17d5cdddb8af1d57a6542f0606f16b053
binisnil <dbsqlssss0704@gmail.com>
2023-10-15 21:14:11 +0900

    [Chore] 프로젝트 파일 연결 및 .gitignore 추가

---
+++ ParkYunBin-assignment/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ParkYunBin-assignment
+//
+//  Created by 박윤빈 on 2023/10/15.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
